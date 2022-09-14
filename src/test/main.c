0052860e8146b7b754740d46d2844b059511bb39
0x404 <69055194+fsb3rke@users.noreply.github.com>
2022-09-14 21:13:41 +0300

    🔥 Updated .c file 🔥
    🌌 Solved syntax error 🌌

--- src/test/main.c
+++ src/test/main.c
@@ -11,5 +11,5 @@ int main(void) {
 
     printf("DAY NUMBER: %d\n", dayNumber(date[0], date[1], date[2]));
     printf("MONTH NAME: %s\n", monthName([date[1]]));
-    printf("NUMBER OF MONTH: %d\n", numberOfMonth([date[1]], date[2]));
+    printf("NUMBER OF MONTH: %d\n", numberOfMonth(date[1], date[2]));
 }
